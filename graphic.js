a1ecfb7c4fefaa94a57b77d1fa2b96c6380d5e50
Muhammad Luthfi Khusyasy <khusyasy@gmail.com>
2021-10-25 15:04:15 +0700

    refactor rename variables lerp function gravity friction colors use hsl

--- graphic.js
+++ graphic.js
@@ -5,45 +5,56 @@ const ctx = canvas.getContext('2d');
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
+const GRAVITY_CONSTANT = 10;
+const FRICTION_CONSTANT = 0.01;
+
 function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 }
 
+function lerp(start, end, amount) {
+  return (1 - amount) * start + amount * end;
+}
+
 class Ball {
-  constructor(x, y, velX, velY, color, size) {
+  constructor(x, y, vx, vy, color, r) {
     this.x = x;
     this.y = y;
-    this.velX = velX;
-    this.velY = velY;
+    this.vx = vx;
+    this.vy = vy;
     this.color = color;
-    this.size = size;
+    this.r = r;
   }
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
     ctx.fill();
   }
   update() {
-    if (this.x + this.size >= width) {
-      this.velX = -this.velX;
+    if (this.x + this.r >= width) {
+      this.vx = -this.vx;
     }
 
-    if (this.x - this.size <= 0) {
-      this.velX = -this.velX;
+    if (this.x - this.r <= 0) {
+      this.vx = -this.vx;
     }
 
-    if (this.y + this.size >= height) {
-      this.velY = -this.velY;
+    if (this.y + this.r >= height) {
+      this.vy = -this.vy;
+    } else {
+      this.vy = lerp(this.vy, GRAVITY_CONSTANT, 0.5);
     }
 
-    if (this.y - this.size <= 0) {
-      this.velY = -this.velY;
+    if (this.y - this.r <= 0) {
+      this.vy = -this.vy;
     }
 
-    this.x += this.velX;
-    this.y += this.velY;
+    this.x = lerp(this.x, this.x + this.vx, 0.5);
+    this.y = lerp(this.y, this.y + this.vy, 0.5);
+    this.vx = lerp(this.vx, 0, FRICTION_CONSTANT);
+    this.vy = lerp(this.vy, 0, FRICTION_CONSTANT);
   }
   collisionDetect() {
     for (let i = 0; i < balls.length; i++) {
@@ -52,47 +63,43 @@ class Ball {
         const dy = this.y - balls[i].y;
         const distance = Math.sqrt(dx * dx + dy * dy);
 
-        if (distance < this.size + balls[i].size) {
-          balls[i].color = this.color =
-            'rgb(' +
-            random(0, 255) +
-            ',' +
-            random(0, 255) +
-            ',' +
-            random(0, 255) +
-            ')';
+        if (distance < this.r + balls[i].size) {
+          let tmpX = balls[i].velX;
+          let tmpy = balls[i].velY;
+          balls[i].velX = this.velX;
+          balls[i].velY = this.velY;
+          this.velX = tmpX;
+          this.velY = tmpy;
         }
       }
     }
-  };
-};
+  }
+}
 
 let balls = [];
 
 while (balls.length < 100) {
-  let size = random(10, 20);
+  let radius = random(10, 20);
   let ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the edge of the canvas, to avoid drawing errors
-    random(0 + size, width - size),
-    random(0 + size, height - size),
+    random(0 + radius, width - radius),
+    random(0 + radius, height - radius),
     random(-7, 7),
     random(-7, 7),
-    'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
-    size
+    `hsl(${random(0, 360)}, 70%, 60%)`,
+    radius
   );
 
   balls.push(ball);
 }
 
 function loop() {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillStyle = 'hsl(0, 0%, 5%)';
   ctx.fillRect(0, 0, width, height);
 
   for (let i = 0; i < balls.length; i++) {
-    balls[i].draw();
-    balls[i].update();
     balls[i].collisionDetect();
+    balls[i].update();
+    balls[i].draw();
   }
 
   requestAnimationFrame(loop);
